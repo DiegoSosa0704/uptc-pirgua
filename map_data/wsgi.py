d94a0f5892c6e3e4561da082e6c70d3870bd9242
dsosa <diego.sosa0704@gmail.com>
2018-11-27 21:27:10 -0500

    Configuración inicial del proyecto.

--- map_data/wsgi.py
+++ map_data/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/ubuntu/uptc-pirgua')
+os.environ['DJANGO_SETTINGS_MODULE'] = "map_data.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "map_data.settings")
+os.environ.setdefault("LANG", "en_US.UTF-8")
+os.environ.setdefault("LC_ALL", "en_US.UTF-8")
+
+# activate = r"/home/ubuntu/uptc-pirgua/env/bin/activate.py"
 
 application = get_wsgi_application()
